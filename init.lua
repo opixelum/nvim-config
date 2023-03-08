ad89d05616eb0c1c1ea7b54ae964e4797193f496
opixelum <48217573+opixelum@users.noreply.github.com>
2023-03-08 14:51:35 +0100

    Refactor, add & sort options to `init.lua`

--- init.lua
+++ init.lua
@@ -1,32 +1,35 @@
+local opt = vim.opt
+
 -- Disable netrw
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+-- Sync with system clipboard
+opt.clipboard = "unnamedplus"
+
+-- Highlight current line
+opt.cursorline = true
+
+-- Use spaces instead of tabs
+opt.expandtab = true
+
+-- Display line numbers
+opt.number = true
+
+-- Use 4 spaces for tabs using spaces
+opt.shiftwidth = 4
+
+-- Insert indents automatically
+opt.smartindent = true
+
+-- Use 4 spaces for softtabstop
+opt.softtabstop = 4
+
+-- Use 4 spaces for tabs
+opt.tabstop = 4
+
+-- Set termguicolors to enable highlight groups
+opt.termguicolors = true
 
 -- Import plugins
 require("plugins")
-
-
--- V I M   S E T T I N G S --
-
--- Use spaces instead of tabs
-vim.opt.expandtab = true
-
--- Use 4 spaces for tabs
-vim.opt.tabstop = 4
-
--- Use 4 spaces for tabs using spaces
-vim.opt.shiftwidth = 4
-
--- Use 4 spaces for softtabstop
-vim.opt.softtabstop = 4
-
--- Display line numbers
-vim.opt.number = true
-
--- Set termguicolors to enable highlight groups
-vim.opt.termguicolors = true
-
-
--- E N A B L E   P L U G I N S --
-require("nvim-tree").setup()
