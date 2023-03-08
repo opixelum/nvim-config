6348a2db4f581cda5364fd21e1f7beeb3bca3acf
opixelum <48217573+opixelum@users.noreply.github.com>
2023-03-08 14:20:42 +0100

    Add AutoSave, update plugins on plugins.lua save & format

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,12 +1,28 @@
-return require('packer').startup(function(use)
+return require("packer").startup(function(use)
     -- Packer
-    use 'wbthomason/packer.nvim'
+    use "wbthomason/packer.nvim"
 
     -- NvimTree
     use {
-        'nvim-tree/nvim-tree.lua',
+        "nvim-tree/nvim-tree.lua",
         requires = {
-          'nvim-tree/nvim-web-devicons'
+          "nvim-tree/nvim-web-devicons"
         }
     }
+
+    -- AutoSave
+    use {
+        "Pocco81/auto-save.nvim",
+        config = function()
+             require("auto-save").setup {}
+        end
+    }
+
+    -- Compile plugins when plugins.lua is saved
+    vim.cmd([[
+        augroup packer_user_config
+	    autocmd!
+	    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+	augroup end
+    ]])
 end)
